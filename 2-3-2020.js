1146841afaece19a7ea3708bb0f7aa47b2e0f217
jeffjlee91 <54928590+jeffjlee91@users.noreply.github.com>
2020-02-03 14:40:55 -0800

    2nd question and 1st failed attempt

--- 2-3-2020.js
+++ 2-3-2020.js
@@ -27,3 +27,20 @@ function toArray(obj) {
 	return Object.keys(obj).map(x => [x, obj[x]]);
 }
 
+
+
+/* Given an array of 10 numbers, return the maximum possible total made by summing just 5 of the 10 numbers.
+
+Examples
+maxTotal([1, 1, 0, 1, 3, 10, 10, 10, 10, 1]) ➞ 43
+
+maxTotal([0, 0, 0, 0, 0, 0, 0, 0, 0, 100]) ➞ 100
+
+maxTotal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]) ➞ 40
+Notes
+N/A */
+
+function maxTotal(nums) {
+        let reducer = (a, b) => a+b;
+        return nums.sort().slice(5).reduce(reducer);
+}
